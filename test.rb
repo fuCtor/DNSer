031fe868a4fbd8220b8d9a7c27462484839213bc
Alexey Shcherbakov <schalexey@gmail.com>
2013-08-03 12:56:35 +0600

    update example

--- test.rb
+++ test.rb
@@ -3,7 +3,13 @@ require "./lib/dnser.rb"
 ipaddr1 = IPAddr.new "3ffe:505:2::1"
 ipaddr2 = IPAddr.new "8.8.8.8"
 
-DNS.domain 'test.ru' do
+
+template :my  do |params|
+  A 'test', params[:ip]
+  MX 'test', params[:ip]
+end
+
+zone 'test.ru' do
   ttl 3600
   ns = NS 'ns1', ipaddr2
   NS 'ns2', ipaddr2
@@ -15,6 +21,8 @@ DNS.domain 'test.ru' do
 
   A host, ipaddr2
   AAAA host, ipaddr1
+
+  apply_my ip: '4.4.4.4'
 
   GOOGLE '6tTalLzrBXBO4Gy9700TAbpg2QTKzGYEuZ_Ls69jle8', :host => 'sub', :comment => "Google App"
 end
